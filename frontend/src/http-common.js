719f9aa4786a137a85364f97f1c075619fc26156
bboriss <borisstevanovic0810@gmail.com>
2022-11-09 20:17:51 +0100

    deployment1

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8800/",
+  baseURL: "https://mern-biketoursnewyork-production.up.railway.app/",
   // headers: {
   //   "Access-Control-Allow-Origin": "*",
   //   "Content-type": "application/json",
